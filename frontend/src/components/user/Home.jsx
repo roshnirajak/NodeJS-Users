768e0350f8b8c13dcc987b1762d4827a156da072
roshnirajak <roshni@gitinteractive.com>
2024-03-21 13:57:08 +0530

    added datatable

--- frontend/src/components/user/Home.jsx
+++ frontend/src/components/user/Home.jsx
@@ -227,7 +227,6 @@ const HomePage = () => {
                             value={courseId}
                             onChange={(e) => {
                                 setCourseId(e.target.value);
-                                // console.log(e.target.value); // This will log the updated value
                             }}
                             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                             required
